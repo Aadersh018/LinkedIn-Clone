a7b684b71caf9c53accb829c6fd674c403638be4
Aadersh018 <aadersh433@gmail.com>
2026-05-31 02:46:15 +0530

    added login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,7 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import generateToken from "../utils/generateToken.js"
+
 export const register = async(req,res)=>{
   try{
     const {firstName,lastName,userName,email,password} = req.body
@@ -7,7 +11,47 @@ export const register = async(req,res)=>{
     if(password.length < 8){
       return res.status(400).json({message:"password should be atleast 8 characters"})
     }
+    const existingUsername = await User.findOne({userName})
+    if(existingUsername){
+      return res.status(400).json({message:"username already exists"})
+    } 
+    const existingUser = await User.findOne({email})
+    if(existingUser){
+      return res.status(400).json({message:"user already exists"})
+    }
+    const hashedPassword = await bcrypt.hash(password,10)
+    const user = await User.create({
+      firstName,
+      lastName,
+      userName,
+      email,
+      password: hashedPassword
+    })
+    return res.status(201).json({message:"new user created"})
   }catch(error){
-    return res.status(400).json({message:"registration error"})
+    console.log(error)
+    return res.status(500).json({message:"registration error"})
+  }
+} 
+
+export const login = async(req,res)=>{
+  try{
+    const{email,password} = req.body
+    if(!email || !password){
+      return res.status(400).json({message:"all fields are required"})
+    }
+    const existingUser = await User.findOne({email})
+    if(!existingUser){
+      return res.status(400).json({message:"user does not exist"})
+    }
+    const isMatch = await bcrypt.compare(password, existingUser.password)
+    if(!isMatch){
+      return res.status(400).json({message:"invalid password"})
+    }
+    const token = generateToken(existingUser._id)
+    return res.status(200).json({message:"user logged in successfully", token})
+  }catch(error){
+    console.log(error)
+    return res.status(500).json({message:`login error ${error}`})
   }
 }
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 import connectDB from "./config/db.js"
+import authRouter from "./routes/auth.route.js"
 
 const app = express()
 let PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use("/api/auth", authRouter)
 
 app.listen(PORT,()=>{
   connectDB()
